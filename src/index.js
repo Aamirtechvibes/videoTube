3c560d7d4553e79d2935fccea6aaae304af4794b
Aamir Khan <vasmo.aamir@gmail.com>
2026-07-18 12:59:27 +0530

    setup project files - part 1

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import app from "./app.js";
+import connectDB from "./db/index.js";
+
+dotenv.config();
+
+export const port = process.env.PORT || 3000;
+
+(async () => {
+    try {
+        await connectDB();
+        app.listen(port, () =>
+            console.log(`🚀 Server is running on http://localhost:${port}`)
+        );
+    } catch (error) {
+        console.error("❌ Failed to connect to database:", error);
+        process.exit(1);
+    }
+})();
