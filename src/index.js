b3105bbc95bc23dd1a903ff1ddb3ad24c1c3ce9f
Aamir Khan <vasmo.aamir@gmail.com>
2026-07-19 01:41:44 +0530

    add utils

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import dotenv from "dotenv";
-
+ 
 import connectDB from "./db/index.js";
+import { error } from "node:console";
 
 
 dotenv.config({
     parth: './.env'
-});
+}); 
 
+const port = process.env.PORT || 3000;
 
-
-connectDB()
+(async () => {
+    try {
+        await connectDB();
+        app.listen(port, () =>
+            console.log(`🚀 Server is running on http://localhost:${port}`)
+        );
+    } catch (error) {
+        console.error("❌ Failed to connect to database:", error);
+        process.exit(1);
+    }
+})();
