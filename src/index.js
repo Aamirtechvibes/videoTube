9a9ba006e745ae10fa5a18719e16a58f5fc5374f
Aamir Khan <vasmo.aamir@gmail.com>
2026-07-20 15:47:44 +0530

    all setup configured

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 import dotenv from "dotenv";
- 
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
-import { error } from "node:console";
+
 
 
 dotenv.config({
-    parth: './.env'
-}); 
+    path: './.env'
+});
 
 const port = process.env.PORT || 3000;
 
 (async () => {
     try {
-        await connectDB();
-        app.listen(port, () =>
-            console.log(`🚀 Server is running on http://localhost:${port}`)
-        );
+         await connectDB();
+
+         const server = app.listen(port, () => {
+             console.log(`🚀 Server is running on http://localhost:${port}`);
+         });
+
+       
     } catch (error) {
         console.error("❌ Failed to connect to database:", error);
         process.exit(1);
